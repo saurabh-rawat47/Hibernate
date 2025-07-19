1c32599f2dc0a08ab4c1b90745764fc884c8e253
saurabh <saurabh.rd80@gmail.com>
2025-07-19 10:56:34 +0530

    update hibernate

--- com/bunny/DemoHib/App.java
+++ com/bunny/DemoHib/App.java
@@ -1,6 +1,7 @@
 package com.bunny.DemoHib;
 
 import java.util.List;
+import java.util.Random;
 
 import org.hibernate.Session;
 import org.hibernate.SessionFactory;
@@ -13,52 +14,108 @@ import org.hibernate.query.Query;
 public class App {
 	public static void main(String[] args) {
 
-		Laptop laptop = new Laptop();
-		laptop.setLid(101);
-		laptop.setLname("HP");
+		/*
+		 * Code for Student1
+		 * 
+		 */
 
-		Student s = new Student();
-		s.setRollno(1);
-		s.setName("Saurabh");
-		s.setMarks(90);
-		s.getLaptop().add(laptop);
-		laptop.getStudent().add(s);
-
-		Configuration config = new Configuration().configure().addAnnotatedClass(Student.class)
-				.addAnnotatedClass(Laptop.class);
+		Configuration config = new Configuration().configure().addAnnotatedClass(Student1.class);
 
 		StandardServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(config.getProperties())
 				.build();
 
 		SessionFactory sf = config.buildSessionFactory(registry);
+		Session session = sf.openSession();
+		session.beginTransaction();
+		/*
+		 * 
+		 * 
+		 * Using HQL(Hibernate Query Language)
+		 * 
+		 */
 
-		Session session1 = sf.openSession();
+//		Query q = session.createQuery("from Student1 where marks > 50");
+//		List<Student1> students = q.list();
+//		for (Student1 s : students) {
+//			System.out.println(s);
+//		}
 
-		session1.beginTransaction();
-		session1.save(laptop);
-		session1.save(s);
+//		Query q = session.createQuery("select rollno,name,marks from Student1");
+//		Object[] student = (Object[]) q.uniqueResult();
+//		for (Object o : student) {
+//			System.out.println(o);
+//		}
 
-		
-		Query query1 = session1.createQuery("from Laptop where lid = 101", Laptop.class);
-		
-		query1.setCacheable(true);
-		Object o1 = query1.uniqueResult(); // Triggers SELECT
-		System.out.println(o1);
-		session1.getTransaction().commit();
-		session1.close();
-		
-		
-		Session session2 = sf.openSession();
-		session2.beginTransaction();
-		Query  query2 = session2.createQuery("from Laptop where lid = 101", Laptop.class);
+		Query q = session.createQuery("select sum(marks) from Student1 where marks > 80");
+		List<Object[]> student = (List<Object[]>) q.list();
+		for (Object[] o : student) {
+			System.out.println(o[0] + ":" + o[1] + ":" + o[2]);
+		}
+//		Random r = new Random();
+//		for (int i = 1; i <= 50; i++) {
+//			Student1 s = new Student1();
+//			s.setRollno(i);
+//			s.setName("Name " + i);
+//			s.setMarks(r.nextInt(100));
+//			session.save(s);
+//		}
 
-		query2.setCacheable(true);
+		session.getTransaction().commit();
 
-		Object o2 = query2.uniqueResult(); // Triggers SELECT
-		System.out.println(o2);
-		session1.getTransaction().commit();
-		session1.close();
-		
+		/*
+		 * Code for student
+		 * 
+		 */
+//		Laptop laptop = new Laptop();
+//		laptop.setLid(101);
+//		laptop.setLname("HP");
+//
+//		Student s = new Student();
+//		s.setRollno(1);
+//		s.setName("Saurabh");
+//		s.setMarks(90);
+//		s.getLaptop().add(laptop);
+//		laptop.getStudent().add(s);
+//
+//		Configuration config = new Configuration().configure().addAnnotatedClass(Student.class)
+//				.addAnnotatedClass(Laptop.class);
+//
+//		StandardServiceRegistry registry = new StandardServiceRegistryBuilder().applySettings(config.getProperties())
+//				.build();
+//
+//		SessionFactory sf = config.buildSessionFactory(registry);
+//
+//		Session session1 = sf.openSession();
+//
+//		session1.beginTransaction();
+//		session1.save(laptop);
+//		session1.save(s);
+//
+//		
+//		Query<Laptop> query1 = session1.createQuery("from Laptop where lid = :id", Laptop.class);
+//		query1.setParameter("id", 101);
+//		query1.setCacheable(true);
+//		Laptop o1 = query1.uniqueResult(); // Triggers SELECT
+//		System.out.println(o1);
+//		session1.getTransaction().commit();
+//		session1.close();
+//		
+//		
+//		Session session2 = sf.openSession();
+//		session2.beginTransaction();
+//		Query<Laptop> query2 = session2.createQuery("from Laptop where lid = :id", Laptop.class);
+//		query2.setParameter("id", 101);
+//		query2.setCacheable(true);
+//
+//		Laptop o2 = query2.uniqueResult(); // Triggers SELECT
+//		System.out.println(o2);
+//		session1.getTransaction().commit();
+//		session1.close();
+
+		/*
+		 * Code for TName Class
+		 */
+
 //		TName tname = new TName();
 //		tname.setFname("Sauarbh");
 //		tname.setMname(".");
---
+++ com/bunny/DemoHib/Student1.java
@@ -0,0 +1,41 @@
+package com.bunny.DemoHib;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+
+@Entity
+public class Student1 {
+	@Id
+	private int rollno;
+	private String name;
+	private int marks;
+
+	public int getRollno() {
+		return rollno;
+	}
+
+	public void setRollno(int rollno) {
+		this.rollno = rollno;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getMarks() {
+		return marks;
+	}
+
+	public void setMarks(int marks) {
+		this.marks = marks;
+	}
+
+	@Override
+	public String toString() {
+		return "Student1 [rollno=" + rollno + ", name=" + name + ", marks=" + marks + "]";
+	}
+}
